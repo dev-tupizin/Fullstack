d6504041b9db38046e0af42788b33e67b1a4dfa8
tupi <dev.tupinamba@gmail.com>
2026-09-14 11:16:47 -0300

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,30 @@
+import { examples } from '@/data/crud';
+import styles from './page.module.css';
+import Card from '@components/Card';
+
+export default function Page() {
+    return (
+        <>
+            <main className={styles.main}>
+                {examples.map(({ id, method, verb, description, color, Icon }) => (
+                    <Card
+                        key={id}
+                        id={id}
+                        verb={verb}
+                        method={method}
+                        description={description}
+                        color={color}
+                        Icon={Icon}
+                    />
+                ))}
+            </main>
+
+            <footer className={styles.footer}>
+                <div>
+                    <p>Codeverse &copy; {new Date().getFullYear()}</p>
+                    <p>Next.js - Axios - Ant Design - Lucide</p>
+                </div>
+            </footer>
+        </>
+    );
+}
---
+++ src/data/crud.js
@@ -0,0 +1,39 @@
+import { KeyRound, Server, HardDrive } from 'lucide-react';
+
+export const examples = [
+    {
+        id: 1,
+        method: 'ApiKey',
+        verb: 'Get',
+        description: 'Lista séries com api-key exposta.',
+        color: 'purple',
+        Icon: KeyRound,
+    },
+    {
+        id: 2,
+        method: 'SSR',
+        verb: 'Get',
+        description: 'Lista séries renderizadas no SSR.',
+        color: 'purple',
+        Icon: Server,
+    },
+    {
+        id: 3,
+        method: 'Offline',
+        verb: 'Get',
+        description: 'Lista séries renderizadas consumida no SessionStorage.',
+        color: 'purple',
+        Icon: HardDrive,
+    }
+];
+
+export const crud = [
+    {
+        id: 1,
+        method: 'ApiKey',
+        verb: 'Get',
+        description: 'Lista series com api-key exposta.',
+        color: 'purple',
+        Icon: KeyRound,
+    },
+];
